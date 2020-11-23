370f2f16123e78a89288ac49d477ad056dc05a58
Creamket_ <66436267+Creamket@users.noreply.github.com>
2020-11-23 09:08:11 +0300

    class => function

--- src/content/Left/Menu/Settings.js
+++ src/content/Left/Menu/Settings.js
@@ -1,31 +1,26 @@
 import { Button, Card } from 'react-bootstrap'
 import { changeTheme } from '../../../redux/actions'
 import { connect } from 'react-redux'
-import React from 'react'
 
-class Settings extends React.Component {
-  render() {
-    return (
-      <Card.Body className='menu-piece'>
-        <Card.Title>Settings</Card.Title>{' '}
-        <Button
-          variant={this.props.themeValue === 'dark' ? 'dark' : 'secondary'}
-          onClick={() =>
-            this.props.onTheme(document.getElementById('theme').classList.contains('dark') ? 'light' : 'dark')
-          }
-          block>
-          - Change theme: {this.props.themeValue === 'dark' ? 'Dark' : 'Light'}
-        </Button>
-      </Card.Body>
-    )
+function Settings({ themeValue, changeTheme }) {
+  function onTheme() {
+    const newTheme = document.getElementById('theme').classList.contains('dark') ? 'light' : 'dark'
+    changeTheme(newTheme)
   }
+
+  return (
+    <Card.Body className='menu-piece'>
+      <Card.Title>Settings</Card.Title>{' '}
+      <Button variant={themeValue === 'dark' ? 'dark' : 'secondary'} onClick={() => onTheme()} block>
+        - Change theme: {themeValue === 'dark' ? 'Dark' : 'Light'}
+      </Button>
+    </Card.Body>
+  )
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  onTheme(newTheme) {
-    dispatch(changeTheme(newTheme))
-  },
-})
+const mapDispatchToProps = {
+  changeTheme,
+}
 
 const mapStateToProps = (state) => {
   return {
