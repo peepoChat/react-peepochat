6d527fa8ff94d4a901d6fc0f3cb30a660e7eb3cd
Creamket_ <66436267+Creamket@users.noreply.github.com>
2020-11-23 19:52:28 +0300

    Credits!

--- src/content/Left/Menu/Credits.js
+++ src/content/Left/Menu/Credits.js
@@ -4,6 +4,28 @@ export default function Credits() {
   return (
     <Card.Body className='menu-piece'>
       <Card.Title> - Credits - </Card.Title>
+      <Card.Subtitle>Development</Card.Subtitle>
+      <Card.Text>
+        {' '}
+        <a href='https://github.com/Creamket' rel='noreferrer' target='_blank'>
+          Creamket_
+        </a>{' '}
+        - frontend <br />{' '}
+        <a href='https://trefis.net/' rel='noreferrer' target='_blank'>
+          trefis
+        </a>{' '}
+        - arrays <br />{' '}
+        <a href='https://github.com/TimaFrolov' rel='noreferrer' target='_blank'>
+          TimaFrolov
+        </a>{' '}
+        - idk
+      </Card.Text>
+      <Card.Subtitle>Sourse code</Card.Subtitle>
+      <Card.Text>
+        <a href='https://github.com/peepoChat' rel='noreferrer' target='_blank'>
+          github
+        </a>
+      </Card.Text>
     </Card.Body>
   )
 }
